7765a5c65bde16ac64619b9a6a685b24bca572b7
simgri2 <61273617+simgri2@users.noreply.github.com>
2020-04-21 15:07:08 +0300

    Create CategoryObj.cs
    Kategorijos objekto klasė

---
+++ CategoryObj.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class CategoryObj : ScriptableObject
+{   
+    public string categoryText;
+    public Sprite categoryImage;
+}
