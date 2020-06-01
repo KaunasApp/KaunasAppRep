d568ab82c7345c86dae87e3f72a9cc77931b33e2
simgri2 <61273617+simgri2@users.noreply.github.com>
2020-06-01 17:26:45 +0300

    Naujausias GameManager.cs update

--- GameManager.cs
+++ GameManager.cs
@@ -9,31 +9,61 @@ public class GameManager : MonoBehaviour {
     public Text question;
     public Image questionImage;
     public Color correctColor, wrongColor, normalCol;
-    public Text progress;
-    public Button atgal;
+    public Text progress; // kelintas dabar atsakinejamas klausimas
+    public Text showScore; //rodomi surinkti taskai siame zaidime
+    public Button backToMenu;
+    public GameObject EndWindow;
+    public GameObject EndBackground;
+    public Text closingText; //kategorijos zaidimo pabaigos tekstas
+    public Button endMenu; //kategorijos pabaiga, grizimas i menu
+    public Button endReplay; //kategorijos pabaiga, zaisti dar karta
+    public ScoreSystem scoreSystem;
+    public Sprite greenBckgr;
+    public Sprite redBackgr;
+    public Sprite neutralBckgr;
 
     private int currentQuestionNr = 0; //einamo klausimo nr
     private List<Klausimas> currentQuestions; //einamos kategorijos klausimai
+    private string currentCategory; //zaidziamos kategorijos pavadinimas
+    private int score;
 
     
-    public void ShowLevel(List<Klausimas> klausimai)
+    public void ShowLevel(List<Klausimas> klausimai, string categoryName)
     {
+        score = 0;
+        currentQuestionNr = 0;
+        EndWindow.SetActive(false);
+        gameObject.SetActive(true);
+        currentCategory = categoryName;
         currentQuestions = klausimai;
         ShowNextQuestion();
+        Debug.Log(currentQuestions.Count + "klausimu count");
+        Debug.Log(scoreSystem.getLevelStatus(currentCategory)+"current category status");
     }
 
     //Grįžti atgal į kategorijų meniu
     public void BackToCategoryMenu()
     {
+        EndWindow.SetActive(false);
         menuManager.gameObject.SetActive(true);
         gameObject.SetActive(false);
+        menuManager.Updt();
+    }
+
+    //žaisti dar kartą
+    public void Replay()
+    {
+        ShowLevel(currentQuestions, currentCategory);
     }
 
     //Klausimo pateikimas
     private void ShowNextQuestion()
     {
+        Debug.Log(currentQuestionNr+"dabartinis klausimas");
+     
         string prog = currentQuestionNr + 1 + " / " + currentQuestions.Count;
         progress.text = prog;
+        showScore.text = "Taškai: " + score.ToString(); 
 
         //jei dar nepasibaige klausimai
         if(currentQuestionNr < currentQuestions.Count)
@@ -65,10 +95,77 @@ public class GameManager : MonoBehaviour {
             // klausimo tekstas
             question.text = currentQuestions[currentQuestionNr].questionInfo;
         }
+
         //atsakyti visi klausimai
         else
         {
-            Debug.Log("baigta");
+            Debug.Log(scoreSystem.getLevelStatus(currentCategory));
+            if(scoreSystem.getLevelStatus(currentCategory) == 2)
+            {
+                if(score > scoreSystem.getLevelScore(currentCategory))
+                {
+                    scoreSystem.setLevelScore(currentCategory, score); //padidino score, bet nebeatrakina naujos
+                    closingText.text = "Kita kategorija jau atrakinta.\n\rPagerinai rezultatą!";
+                    EndBackground.GetComponent<SpriteRenderer>().sprite = neutralBckgr;
+                    EndWindow.SetActive(true);
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    closingText.text = "Kita kategorija jau atrakinta";
+                    EndBackground.GetComponent<SpriteRenderer>().sprite = neutralBckgr;
+                    EndWindow.SetActive(true);
+                    gameObject.SetActive(false);
+                }
+            }
+            else if(scoreSystem.getLevelStatus(currentCategory) == 1)
+            {
+                if(score > scoreSystem.getLevelScore(currentCategory))
+                {
+                    scoreSystem.setLevelScore(currentCategory, score); //issaugoju nauja score
+
+                    if (scoreSystem.getLevelStatus(currentCategory) == 2) //atrakina nauja kategorija
+                    {
+                        if(currentCategory == "Skulptūros")
+                        {
+                            gameObject.SetActive(false);
+                            closingText.text = "Įveikėte visas kategorijas!";
+                            EndBackground.GetComponent<SpriteRenderer>().sprite = greenBckgr;
+                            EndWindow.SetActive(true);
+                            gameObject.SetActive(false);
+                        }
+                        else
+                        {
+                            gameObject.SetActive(false);
+                            closingText.text = "Atrakinta nauja kategorija!";
+                            EndBackground.GetComponent<SpriteRenderer>().sprite = greenBckgr;
+                            EndWindow.SetActive(true);
+                            gameObject.SetActive(false);
+                            scoreSystem.UnlockNewCategory();
+                        }
+                    }
+                    else
+                    {
+                        //pagerintas score, bet neatrakinta nauja kategorija
+                        closingText.text = "Pagerinai rezultatą!\n\rDeja, nepakanka taškų naujai kategorijai atrakinti";
+                        EndBackground.GetComponent<SpriteRenderer>().sprite = neutralBckgr;
+                        EndWindow.SetActive(true);
+                        gameObject.SetActive(false);
+                    }
+                }
+                else
+                {
+                    //neatrakinta nauja kategorija, nepagerino score
+                    closingText.text = "Nepakanka taškų naujai kategorijai atrakinti.\n\rBandyk dar kartą!";
+                    EndBackground.GetComponent<SpriteRenderer>().sprite = redBackgr;
+                    EndWindow.SetActive(true);
+                    gameObject.SetActive(false);
+                }      
+            }
+            else
+            {
+                Debug.Log("Klaida, levelSatus = 0, negali būti");
+            }
         }
     }
 
@@ -91,16 +188,16 @@ public class GameManager : MonoBehaviour {
 
         if (btn.GetComponentInChildren<Text>().text == currentQuestions[currentQuestionNr].correctAns)
         {
-            Debug.Log("MLDC");
+            //pasirinktas teisingas atsakymas
             Color col = btn.image.color;
             col = correctColor;
             col.a = 1f;
             btn.image.color = col;
-
+            score += 10;
         }
         else
         {
-            Debug.Log("YOU SUCK");
+            //pasirinktas neteisingas atsakymas
             Color col = btn.image.color;
             col = wrongColor;
             col.a = 1f;
