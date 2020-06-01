03cde5e3161f8d837d2d31e4ca92665acbb1b2e4
simgri2 <61273617+simgri2@users.noreply.github.com>
2020-06-01 17:28:07 +0300

    Naujausias MenuManager.cs update

--- MenuManager.cs
+++ MenuManager.cs
@@ -7,17 +7,29 @@ public class MenuManager : MonoBehaviour {
 
     public Klausimas[] klausimai;
     public GameManager gameManager;
+    public ScoreSystem scoreSystem;
     public CategoryObj[] kategorijos;
     public Button[] categories;
+    public Text currentScore;
+
     private void Start()
     {
+        //scoreSystem.Destroy(); //ISTRINA VISUS PLAYERPREFS PRIES ZAIDIMA
         for(int i = 0; i < kategorijos.Length; i++)
         {
-            //gameManager.gameObject.SetActive(false);
-            //gameObject.SetActive(true);
+            gameManager.gameObject.SetActive(false);
+            gameObject.SetActive(true);
             categories[i].image.sprite = kategorijos[i].categoryImage;
             categories[i].transform.GetComponentInChildren<Text>().text = kategorijos[i].categoryText;
-        }      
+        }
+        ScorePresentation();
+        LevelPresentation();
+    }
+
+    public void Updt()
+    {
+        ScorePresentation();
+        LevelPresentation();
     }
 
     //nurodytos kategorijos klausimu atrinkimas
@@ -32,8 +44,7 @@ public class MenuManager : MonoBehaviour {
                 atrinktiKlausimai.Add(question);
             }
         }
-        Debug.Log(atrinktiKlausimai.Count);
-        gameManager.ShowLevel(atrinktiKlausimai);
+        gameManager.ShowLevel(atrinktiKlausimai, chosenCategory.ToString());
         gameManager.gameObject.SetActive(true);
         gameObject.SetActive(false);
     }
@@ -49,11 +60,11 @@ public class MenuManager : MonoBehaviour {
                 PickOutQuestions(categ);
                 break;
             case "Gatvės menas":
-                categ = CategoryEnum.Category.GatvesMenas;
+                categ = CategoryEnum.Category.GatvėsMenas;
                 PickOutQuestions(categ);
                 break;
             case "Architektūra":
-                categ = CategoryEnum.Category.Architektura;
+                categ = CategoryEnum.Category.Architektūra;
                 PickOutQuestions(categ);
                 break;
             case "Muziejai":
@@ -65,12 +76,34 @@ public class MenuManager : MonoBehaviour {
                 PickOutQuestions(categ);
                 break;
             case "Skulptūros":
-                categ = CategoryEnum.Category.Skulpturos;
+                categ = CategoryEnum.Category.Skulptūros;
                 PickOutQuestions(categ);
                 break;
             default:
                 Debug.Log("Klaida atpazistant kategorija. Gauta: "+ chosenCategory);
                 break;
+        }
+    }
+
+    public void ScorePresentation()
+    {
+        scoreSystem.checkIfInitialised();
+        int score = scoreSystem.getScoreSum();
+        currentScore.text = "Taškai: " + score.ToString();
+    }
+
+    public void LevelPresentation()
+    {
+        string[] categoryArray = { "MiestoAkcentai", "GatvėsMenas", "Architektūra", "Muziejai", "Istorija", "Skulptūros" };
+        int i = 0;
+        foreach(string name in categoryArray)
+        {
+            if(scoreSystem.getLevelStatus(name) > 0)
+            {
+                categories[i].interactable = true;
+            }
+            else if(scoreSystem.getLevelStatus(name) == 0) categories[i].interactable = false;
+            i++;
         }
     }
 
