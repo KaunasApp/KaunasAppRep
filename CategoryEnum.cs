d7ab73a124d728897b71ab5cb4c71b0985b11fed
simgri2 <61273617+simgri2@users.noreply.github.com>
2020-06-01 17:29:30 +0300

    Create CategoryEnum.cs

---
+++ CategoryEnum.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CategoryEnum : MonoBehaviour {
+
+    
+    public enum Category
+    {
+        MiestoAkcentai,
+        Istorija,
+        GatvėsMenas,
+        Architektūra,
+        Muziejai,
+        Skulptūros
+    }
+}
